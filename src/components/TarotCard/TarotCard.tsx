85392a516b87f2a35e01176e98af31651c616df4
kylesezhi <kyle.bedell@gmail.com>
2023-12-12 17:10:57 -0500

    Add animation

--- src/components/TarotCard/TarotCard.tsx
+++ src/components/TarotCard/TarotCard.tsx
@@ -1,6 +1,7 @@
 import React from 'react';
 import './TarotCard.css';
 import { useSpring, animated } from '@react-spring/web';
+import backImage from './assets/78.webp';
 
 interface TarotProps {
   number: number,
@@ -10,11 +11,31 @@ interface TarotProps {
 
 function TarotCard({number = 78, reversed = false, isShowing}: TarotProps) {
   const tarotImage = require(`./assets/${number}.webp`);
-  const style = {
-    transform: reversed ? 'rotate(180deg)' : 'rotate(0deg)',
+  const { transform, opacity } = useSpring({
+    opacity: !isShowing ? 1 : 0,
+    transform: `perspective(900px) rotateY(${!isShowing ? 180 : 0}deg)`,
+    config: { mass: 10, tension: 500, friction: 80 },
+  });
+  const frontStyle = {
+    opacity: opacity.to((o) => 1 - o),
+    transform
   };
+  const backStyle = {
+    opacity,
+    transform,
+    rotateX: "180deg",
+  }
+  const reversedStyle = {
+    transform: `rotate(${reversed ? '180' : '0'}deg)`,
+  }
+
   return (
-    <animated.img src={tarotImage} className="tarot-card" style={style} />
+    <div className="tarot-cards-container">
+      <animated.div className="tarot-card" style={frontStyle}>
+        <img src={tarotImage} className="tarot-image" alt="tarot-card" style={reversedStyle} />
+      </animated.div>
+      <animated.img src={backImage} className="tarot-card" style={backStyle} />
+    </div>
   )
 }
 
