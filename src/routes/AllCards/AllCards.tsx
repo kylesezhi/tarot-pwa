af180cc96237a0b9fecca38249a43b11832bc788
kylesezhi <kyle.bedell@gmail.com>
2023-12-21 21:21:32 -0500

    Add back arrow to all cards

--- src/routes/AllCards/AllCards.tsx
+++ src/routes/AllCards/AllCards.tsx
@@ -1,11 +1,16 @@
 import "./AllCards.css";
 import { useTarotStore } from "../../store";
 import StaticTarotCardContainer from "../../containers/StaticTarotCardContainer/StaticTarotCardContainer";
+import TopNavigation from "../../components/TopNavigation/TopNavigation";
+import BackChevronLinkContainer from "../../containers/BackChevronLinkContainer/BackChevronLinkContainer";
 
 function AllCards() {
   const interpretations = useTarotStore((state) => state.interpretations);
   return (
     <div className="all-cards">
+      <TopNavigation>
+        <BackChevronLinkContainer />
+      </TopNavigation>
       {interpretations.map((card, number) => (
         <StaticTarotCardContainer
           key={`${number}${card.name}`}
