7e1372f710cc9b438fbedac530a4f0cb5e8a3b48
kylesezhi <kyle.bedell@gmail.com>
2024-01-07 20:19:31 -0500

    Attempt to fix lag on update w workbox

--- TODO.md
+++ TODO.md
@@ -1,6 +1,8 @@
 # TODO
 
 1. Inform user they can update with badge, update button
+  - https://stackoverflow.com/questions/55653236/access-service-worker-skipwaiting-from-within-app-build-with-webpackworkbox
+  - https://stackoverflow.com/questions/52904430/how-to-implement-skipwaiting-with-create-react-app
 1. Write integration tests
 1. Start with infinity symbol, fade out to card fading in
 1. 3D for card flipping - https://codesandbox.io/p/sandbox/enter-portals-9m4tpc?file=%2Fsrc%2Findex.js
--- src/index.tsx
+++ src/index.tsx
@@ -18,7 +18,17 @@ root.render(
 // If you want your app to work offline and load faster, you can change
 // unregister() to register() below. Note this comes with some pitfalls.
 // Learn more about service workers: https://cra.link/PWA
-serviceWorkerRegistration.register();
+serviceWorkerRegistration.register({
+  onUpdate: (registration) => {
+    registration.unregister().then(() => {
+      window.location.reload();
+    });
+  },
+  onSuccess: (registration) => {
+    console.info("service worker on success state");
+    console.log(registration);
+  },
+});
 
 // If you want to start measuring performance in your app, pass a function
 // to log results (for example: reportWebVitals(console.log))
